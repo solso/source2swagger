b0e0af3c3c58c65eb37df038bd9dec7e4558df04
Tony Tam <fehguy@gmail.com>
2013-02-06 19:18:01 -0800

    updated swagger version per #9

--- test/data/sample6.rb
+++ test/data/sample6.rb
@@ -1,4 +1,4 @@
 ##~ sapi = source2swagger.namespace("sentiment")
 ##~ sapi.basePath = "<%= @base_path %>"
-##~ sapi.swagrVersion = "0.1a"
+##~ sapi.swaggerVersion = "0.1a"
 ##~ sapi.apiVersion = "1.0"
